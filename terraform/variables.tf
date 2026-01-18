4878351bb2497f308b9557fdf47ec63ce08f0441
Sanskruti-Thakur <sanskrutithakur00@gmail.com>
2026-01-18 05:38:06 +0530

    Set default CIDR for allowed SSH access

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,5 +2,5 @@
 variable "allowed_ssh_cidrs" {
   description = "List of CIDR blocks allowed for SSH access"
   type        = list(string)
-  default     = []
+  default     = ["49.36.99.58/32"]  
 }
