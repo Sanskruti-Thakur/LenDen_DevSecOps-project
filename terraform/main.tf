96bf377042efb5272a26f5de3e9ee460827f0eab
Sanskruti-Thakur <sanskrutithakur00@gmail.com>
2026-01-18 05:43:10 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# VPC
+variable "allowed_ssh_cidrs" {
+  description = "List of CIDR blocks allowed for SSH access"
+  type        = list(string)
+  default     = []
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   
@@ -22,7 +27,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Public Subnet
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
@@ -32,7 +36,6 @@ resource "aws_subnet" "public" {
   }
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   
@@ -41,7 +44,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Security Group
 resource "aws_security_group" "web_sg_secure" {
   name        = "lenden-secure-sg"
   description = "Secure SG after AI remediation"
@@ -64,39 +66,29 @@ resource "aws_security_group" "web_sg_secure" {
   }
 
   egress {
-    description = "Allow all outbound"  # Optional: Restrict if possible
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Allow HTTP/HTTPS outbound"
+    from_port   = 80
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name       = "secure-sg"
-    Remediated = "true"
-    Student    = "Sanskruti-Thakur"
+    Name        = "secure-sg"
+    Remediated  = "true"
+    Student     = "Sanskruti-Thakur"
   }
 }
 
-# EC2 Instance
+
 resource "aws_instance" "web_server" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Ubuntu 22.04 in ap-south-1
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
+  ami                    = "ami-0f5ee92e2d63afc18"  # Ubuntu 22.04
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg_secure.id]
-  key_name      = "lenden-key"
-
-  # IMDS v2 enforced
-  metadata_options {
-    http_tokens = "required"
-  }
-
-  # Root volume encryption
-  root_block_device {
-    encrypted = true
-  }
-
-  # User data to deploy docker container
+  
+  key_name = "lenden-key"
+  
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
@@ -109,40 +101,30 @@ resource "aws_instance" "web_server" {
   tags = {
     Name = "lenden-web-server"
   }
+
+  # Enable IMDSv2 for security
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
 }
 
-# Elastic IP
 resource "aws_eip" "web_ip" {
   instance = aws_instance.web_server.id
-  vpc      = true
 }
 
-# VPC Flow Logs
-resource "aws_iam_role" "flow_log_role" {
-  name = "vpc-flow-log-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "vpc-flow-logs.amazonaws.com" }
-    }]
-  })
-}
 
-resource "aws_iam_role_policy_attachment" "flow_log_attach" {
-  role       = aws_iam_role.flow_log_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonVPCFlowLogsRole"
+resource "aws_cloudwatch_log_group" "vpc_flow" {
+  name              = "/aws/vpc/lenden-vpc"
+  retention_in_days = 14
 }
 
 resource "aws_flow_log" "vpc_flow" {
-  vpc_id        = aws_vpc.main.id
-  traffic_type  = "ALL"
-  log_group_name = "/aws/vpc/lenden-vpc"
-  iam_role_arn   = aws_iam_role.flow_log_role.arn
+  vpc_id          = aws_vpc.main.id
+  traffic_type    = "ALL"
+  log_destination = aws_cloudwatch_log_group.vpc_flow.arn
 }
 
-# Output
-output "application_url" {
-  value = "http://${aws_eip.web_ip.public_ip}:5000"
-}
